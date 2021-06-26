47bfdeddce14fe80bd1702314fd5880dc97ec0f8
Mizanur Rahman <mizzanur007@gmail.com>
2021-06-26 22:56:44 +0600

    finished

--- index.js
+++ index.js
@@ -7,9 +7,12 @@ const userRoute = require('./routes/users');
 const postRoute = require('./routes/posts');
 const categoryRoute = require('./routes/categories');
 const multer = require('multer');
+const path = require('path')
 
 dotenv.config();
 app.use(express.json());
+app.use('/images',express.static(path.join(__dirname, '/images'))) 
+
 
 const port = process.env.PORT || 80;
 const uri = `mongodb+srv://blogMizan:blogMizan@cluster0.yk7ln.mongodb.net/blog?retryWrites=true&w=majority`
